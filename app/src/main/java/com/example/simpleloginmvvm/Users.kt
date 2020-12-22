9cfdd678bcbf856bdd2f107fed95179bb28993e9
SAFE <safesai@hotmail.com>
2020-12-21 17:28:15 -0800

    one

---
+++ app/src/main/java/com/example/simpleloginmvvm/Users.kt
@@ -0,0 +1,15 @@
+package com.example.simpleloginmvvm
+
+import org.jetbrains.exposed.sql.Table
+
+object Users : Table("user") {
+
+    val user_id = integer("user_id").autoIncrement()
+    val username = varchar("username", 50)
+    val password = varchar("password", 10)
+    val name = varchar("name", 50)
+
+    override val primaryKey: PrimaryKey?
+        get() = PrimaryKey(user_id, name = "user_id")
+
+}
